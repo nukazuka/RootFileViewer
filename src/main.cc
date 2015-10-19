90f1b97fc7df4f8a32029e382f3e3bdcc01499da
Genki Nukazuka <nukadukagenki@gmail.com>
2015-10-19 14:58:46 +0200

    minor modification

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@ int main( int argc, char* argv[] )
 {
 
   Argument* arg = new Argument( argc, argv );
-  //  gErrorIgnoreLevel = 5000;
+  gErrorIgnoreLevel = 5000;
 
   TApplication* app = new TApplication( "app" , &argc, argv );
   Drawer* dr = new Drawer( arg );
